87a222bec1cfe849dc3b9a9b0b102aa51f01ef0b
Shon Santhosh <shonsanthosh1977@gmail.com>
2021-11-16 14:51:00 +0530

    Fixed bugs

--- Lecture3/settings.py
+++ Lecture3/settings.py
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'hello',
     'itsnewyear',
-    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
